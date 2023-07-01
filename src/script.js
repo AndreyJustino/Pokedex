927933d2cc9f4838e9ed6bbf45eda415b95b9183
AndreyJustino <100535840+AndreyJustino@users.noreply.github.com>
2023-06-30 21:21:35 -0300

    mostrando pokemon

--- src/script.js
+++ src/script.js
@@ -1,20 +1,36 @@
 async function buscaPokemon() {
     try {
+        const busca = document.getElementById('pesquisa').value
+
         // conectando a api
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${busca}`);
         const pokemonData = await response.json();
 
 
         // pegando dados da api
         const id = pokemonData.id
-        const name = pokemonData.forms[0].name
-        const img = `raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+        const name = pokemonData.name
+        const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
         
+        const pokedexElement = document.getElementById('pokedex');
+
+        const pokemonCard = document.createElement('div');
+        pokemonCard.classList.add('pokemon-card');
+      
+        const pokemonName = document.createElement('p');
+        pokemonName.classList.add('pokemon-name');
+        pokemonName.innerText = name
+
+        const pokemonImage = document.createElement('img');
+        pokemonImage.classList.add('pokemon-image');
+        pokemonImage.src = img
+
+        pokemonCard.appendChild(pokemonName);
+        pokemonCard.appendChild(pokemonImage);
+        pokedexElement.appendChild(pokemonCard);
         
     } catch (error) {
       console.log(error);
     }
   }
-  
-  buscaPokemon();
   
