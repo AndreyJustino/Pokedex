529fc34d3c0bdf96b31a07a88211a19f5cb3d841
AndreyJustino <100535840+AndreyJustino@users.noreply.github.com>
2023-07-08 23:21:13 -0300

    buscar pokemon

--- src/script.js
+++ src/script.js
@@ -1,36 +1,47 @@
 let c = 20
+let i = 0
 
 async function buscaPokemon() {
     try {
         const busca = document.getElementById('pesquisa').value
 
-        // conectando a api
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${busca}`);
-        const pokemonData = await response.json();
+        if(busca == ''){
+            alert('Digite o nome de um pokemon!')
+        } else{
+            // conectando a api
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${busca}`);
+            const pokemonData = await response.json();
 
-        // pegando dados da api
-        const id = pokemonData.id
-        const name = pokemonData.name
-        const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+            // pegando dados da api
+            const id = pokemonData.id
+            const name = pokemonData.name
+            const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+            
+            //criando elemento
+            const pokedexElement = document.getElementById('pokemonBusca');
+
+            if(i > 0){
+                pokedexElement.removeChild(pokedexElement.firstChild)
+            }
+
+            const pokemonCard = document.createElement('div');
+            pokemonCard.classList.add('pokemon-card-busca');
         
-        //criando elemento
-        const pokedexElement = document.getElementById('pokedex');
+            const pokemonName = document.createElement('p');
+            pokemonName.classList.add('pokemon-name');
+            pokemonName.innerText = name
 
-        const pokemonCard = document.createElement('div');
-        pokemonCard.classList.add('pokemon-card');
-      
-        const pokemonName = document.createElement('p');
-        pokemonName.classList.add('pokemon-name');
-        pokemonName.innerText = name
+            const pokemonImage = document.createElement('img');
+            pokemonImage.classList.add('pokemon-image');
+            pokemonImage.src = img
+            
+            // exibindo
+            pokemonCard.appendChild(pokemonName);
+            pokemonCard.appendChild(pokemonImage);
+            pokedexElement.appendChild(pokemonCard);
 
-        const pokemonImage = document.createElement('img');
-        pokemonImage.classList.add('pokemon-image');
-        pokemonImage.src = img
-
-        // exibindo
-        pokemonCard.appendChild(pokemonName);
-        pokemonCard.appendChild(pokemonImage);
-        pokedexElement.appendChild(pokemonCard);
+            i++
+        }
         
     } catch (error) {
       console.log(error);
